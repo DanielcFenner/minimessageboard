c178dd94fcbe90896a59291a8a43e81e696b74e5
DanielFenner <dcfenner@gmail.com>
2022-07-01 11:52:54 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+var express = require("express");
+var router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages });
+});
+
+router.get("/form", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  let messageText = req.body.message;
+  let messageUser = req.body.name;
+
+  messages.push({
+    text: messageText,
+    user: messageUser,
+    added: new Date(),
+  });
+
+  res.redirect("/");
+});
+
+module.exports = router;
